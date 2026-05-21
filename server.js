ae889335934b91c2fc133ea64fc3a984fe503378
PRASATH <saprasath15@gmail.com>
2026-05-21 18:14:01 +0530

    updates

--- server.js
+++ server.js
@@ -31,12 +31,10 @@ app.use('/api/qr', qrRoutes);
 app.use('/api/payments', paymentRoutes);
 
 // Monolithic Setup - Serve React App
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client', 'dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-    });
-}
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
